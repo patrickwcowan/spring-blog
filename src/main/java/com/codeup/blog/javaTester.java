eca9871339e2b2d11d4af38356980f84ce4dd42b
Parick Cowan <patrick.w.cowan@gmail.com>
2019-05-13 10:34:05 -0500

    fixed cods

--- src/main/java/com/codeup/blog/javaTester.java
+++ src/main/java/com/codeup/blog/javaTester.java
@@ -1,89 +1,32 @@
-//package com.codeup.blog;
+//import java.util.Arrays;
 //
-//import java.util.ArrayList;
+//public class javaTester{
 //
-//public class javaTester {
-//
-////public static String tickets(int[] input){
-////    String response = "";
-////    int[] drawer = new int[input.length];
-////
-//////    for (int amount : input) {
-//////
-//////        if (amount == 25) {
-//////            drawer+=amount;
-//////        } else if (amount == 50) {
-//////            drawer+=amount;
-//////            drawer = drawer - 25;
-//////        } else if (amount == 100) {
-//////            drawer+=amount;
-//////            drawer= drawer - 75;
-//////        }
-//////
-//////    }
-////
-////    for (int i = 0; i < input.length;i++) {
-////        if (input[i] == 25) {
-////            drawer[i] = input[i];
-////
-////
-////        } else if (input[i] == 50) {
-////            for (int amount : drawer) {
-////                if (amount == 25){
-////                    drawer[i] = input[i];
-////                }
-////            }
-////
-////
-////
-////        } else if (input[i] == 100) {
-////            for (int amount : drawer) {
-////                if (amount ==)
-////            }
-////            drawer[i] = input[i];
-////        }
-////    }
-////
-////    for (int number : drawer) {
-////        System.out.println(number);
-////    }
-////
-////    return response;
-////}
-//
-//
-//    public static double findUniq(double[] input) {
-//        ArrayList<Double> newList = new ArrayList<>();
-//        double unique = 0.0;
-//        for (int i = 0; i < input.length; i++) {
-//            newList.add(i,input[i]);
-//            for ()
-//
-//
-//
-//        }
-//
-//        return  unique;
+//    public static void sayHello(){
+//        System.out.println("Hi");
 //    }
+//
+//
+//    public static long findNextSquare(int input){
+//        int output = 0;
+//        input % 555555555555555555555555
+//        return output;
+//    }
+//
+//
+//
+//
+//
+//
 //
 //
 //
 //    public static void main(String[] args) {
 //
 //
-//    double[] line1 = {25,25,50,50,67,67,35};
-//    double[] line2 = {25,100,25};
-//    double[] line3 = {25, 25, 50, 50, 100};
-//
-//        System.out.println(findUniq(line1));
 //
 //
 //
 //
 //
-//
-//
-//
-//
-//    }
-//}
+//}}
